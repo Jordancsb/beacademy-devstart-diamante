334fd04a168536935f8bc02c62dfb0cf4a0fa9c7
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-07-01 11:20:50 -0300

    Criacao de rotas para UserAccess e AdminAccess

--- devstart/routes/web.php
+++ devstart/routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/** Login Page Route */
+Route::get('/',[UserController::class,'login'])->name('login.page');
+Route::post('/auth',[UserController::class,'auth'])->name('auth.user');
+
+
+/** Middleware for redirect admin/users */
+Route::middleware(['admin'])->group( function (){
+
+    Route::get('admin',function(){      // all routes for the admin is put here
+        
+        dd("Voce é um administrador");
+    });
+});
+
+Route::middleware(['user'])->group( function (){
+    
+    Route::get('user',function(){           // all routes for the user is put here
+        
+        dd("Voce é um Usuario padrão");
+    });
 });
