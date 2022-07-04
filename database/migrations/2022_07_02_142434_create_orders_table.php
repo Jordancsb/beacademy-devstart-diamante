e2753c6ab3478f5868eb0b0ae05560034f6fa817
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 12:57:34 -0300

    fix: padronizado para uso do underline

--- database/migrations/2022_07_02_142434_create_orders_table.php
+++ database/migrations/2022_07_02_142434_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('client_id');
             $table->integer('product_id');
-            $table->integer('productQuantity');
+            $table->integer('product_quantity');
             $table->string('status');
             $table->timestamps();
         });
