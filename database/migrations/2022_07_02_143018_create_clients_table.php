0607483e917d767728d3c185b1672a76775a7831
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 13:11:03 -0300

    fix: tabelas foram padronizadas

--- database/migrations/2022_07_02_143018_create_clients_table.php
+++ database/migrations/2022_07_02_143018_create_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('client', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('CPF');
+            $table->string('cpf');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
