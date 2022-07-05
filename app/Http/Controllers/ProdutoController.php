b9c88b4e063a7138622fd5328fb82b863b108170
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-07-05 14:53:36 -0300

    Pagina de cadastro de Produto, criado controller de Produto e rotas

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function cadastro()
+    {
+        return view("pages.cadastro");
+    }
+
+    public function cadastroProduto(Request $request){
+        $produto = $request->all();
+        dd($produto);
+    }
+}
---
+++ resources/views/pages/cadastro.blade.php
@@ -0,0 +1,43 @@
+@extends('layout.modelo')
+
+@section('title')
+    Cadastro de Produtos
+@endsection
+
+@section('content')
+    <section class="container d-flex mt-4 ">
+        <form action="{{route('cadastro')}}" class="form-cadastro-produto" method="POST">
+            @csrf
+            <div class="form-group">
+               <label for="name">Nome Completo</label>
+               <input type="text" class="form-control" id="name">
+            </div>
+            <div class="form-group">
+               <label for="description">Descrição</label>
+               <input type="text" class="form-control" id="description"  >
+            </div>
+            <div class="form-group">
+               <label for="image_url">URL Imagem</label>
+               <input type="text" id="image_url" class="form-control"  >
+            </div>
+            <div class="form-group">
+               <label for="size">Tamanho</label>
+               <input type="text" id="size" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label for="quantity">Quantidade</label>
+                <input type="text" id="quantity" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label for="sale_price">Preço de Venda</label>
+                <input type="text" id="sale_price" class="form-control" >
+            </div>
+            <div class="form-group">
+                <label for="cost_price">Preço de Custo</label>
+                <input type="text" id="cost_price" class="form-control" >
+            </div>
+            <button type="submit" class="btn btn-black">Cadastrar</button>
+            <button type="submit" class="btn btn-black">Cancelar</button>
+         </form>
+    </section>
+@endsection
