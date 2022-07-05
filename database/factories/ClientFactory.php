269880cbc8eb77303ea5decbf108d887ce6bf7a8
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-05 16:16:19 -0300

    feat: factory dos clientes funcionando

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'cpf' => $this->faker->numberBetween(10000000000, 99999999999),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->email(),
+            'phone' => $this->faker->phoneNumber(),
+            'login' => $this->faker->userName(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\Product::factory(20)->create();
 
+        \App\Models\Client::factory(10)->create();
+
     // \App\Models\User::factory()->create([
     //     'name' => 'Test User',
     //     'email' => 'test@example.com',
