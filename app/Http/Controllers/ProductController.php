84e12c75bda238e83e795510703cf29e27694b84
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-07-06 14:39:50 -0300

    Tela para simples listagem de produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Symfony\Component\HttpFoundation\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -23,5 +24,14 @@ class ProductController extends Controller
         $this->model->create($data);
 
         return redirect()->route('product.new');
+    }
+
+    public function listProduct() {
+        $products = DB::select('select * from products');
+        return view('product.list')->with('products',$products);
+    }
+
+    public function cartProducts(Request $request, $id){
+            // add the products that were selected by ID
     }
 }
