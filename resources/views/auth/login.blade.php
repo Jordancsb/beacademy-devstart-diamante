24d432248ad55b69fcb5ab4b4fdc43953928f580
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-06 14:09:53 -0300

    fix: bootstrap script afuncional

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ ('/css/app.css') }}" rel="stylesheet">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-  <title>Login</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="{{ '/css/app.css' }}" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<title>Login</title>
 </head>
+
 <body>
-<div class="sidenav">
-         <div class="login-main-text">
-            <h2>Faça seu login</h2>
-            <p>Ainda não faz parte do nosso time! Registre-se agora!</p>
-         </div>
-         <img class="img" src="{{URL::asset('/assets/logo.png')}}" alt="profile Pic" height="200" width="200">
-      </div>
-      <div class="main">
-         <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-               <form>
-                  <div class="form-group">
-                     <label>Usuário/Email</label>
-                     <input type="text" class="form-control" placeholder="Usuário">
-                  </div>
-                  <div class="form-group">
-                     <label>Senha</label>
-                     <input type="password" class="form-control" placeholder="Senha">
-                  </div>
-                  <button type="submit" class="btn btn-black"><a class="btn-black" href="/">Acesse</a></button>
-                  <button type="submit" class="btn btn-black"><a class="btn-black" href="/register">Cadastre-se</a></button>
-               </form>
-            </div>
-         </div>
-      </div>
+	<div class="sidenav">
+		<div class="login-main-text">
+			<h2>Faça seu login</h2>
+			<p>Ainda não faz parte do nosso time! Registre-se agora!</p>
+		</div>
+		<img class="img" src="{{ URL::asset('/assets/logo.png') }}" alt="profile Pic" height="200" width="200">
+	</div>
+	<div class="main">
+		<div class="col-md-6 col-sm-12">
+			<div class="login-form">
+				<form>
+					<div class="form-group">
+						<label>Usuário/Email</label>
+						<input type="text" class="form-control" placeholder="Usuário">
+					</div>
+					<div class="form-group">
+						<label>Senha</label>
+						<input type="password" class="form-control" placeholder="Senha">
+					</div>
+					<button type="submit" class="btn btn-black"><a class="btn-black" href="/">Acesse</a></button>
+					<button type="submit" class="btn btn-black"><a class="btn-black" href="/register">Cadastre-se</a></button>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </body>
+
 </html>
