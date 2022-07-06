fa5b11015d4dbeab4866d1d808de289adecb1875
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-06 17:22:40 -0300

    fix: login e register view com html puro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,32 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+@extends('template.default', ['navbar' => false])
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="./css/app.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@section('title', 'Login')
 
-	<title>Login</title>
-</head>
-
-<body>
-	<div class="sidenav">
-		<div class="login-main-text">
-			<h2>Faça seu login</h2>
-			<p>Ainda não faz parte do nosso time! Registre-se agora!</p>
+@section('content')
+	<section>
+		<div class="sidenav">
+			<div class="login-main-text">
+				<h2>Faça seu login</h2>
+				<p>Ainda não faz parte do nosso time! Registre-se agora!</p>
+			</div>
+			<img class="img" src="./assets/logo.png" alt="profile Pic" height="200" width="200">
 		</div>
-		<img class="img" src="./assets/logo.png" alt="profile Pic" height="200" width="200">
-	</div>
-	<div class="main">
-		<div class="col-md-6 col-sm-12">
-			<form class="login-form" action="" method="POST">
-				@csrf
-				<div class="form-group">
-					<label>Email</label>
-					<input type="text" class="form-control" placeholder="Email@email.com" name="email" required>
-				</div>
-				<div class="form-group">
-					<label>Senha</label>
-					<input type="password" class="form-control" placeholder="********" name="password" required>
-				</div>
-				<button type="submit" class="btn btn-black">Acesse</button>
-				<a class="btn btn-black" href="{{ route('register.page') }}">Cadastre-se</a>
-			</form>
+		<div class="main">
+			<div class="col-md-6 col-sm-12">
+				<form class="login-form" action="" method="POST">
+					@csrf
+					<div class="form-group">
+						<label>Email</label>
+						<input type="text" class="form-control" placeholder="Email@email.com" name="email" required>
+					</div>
+					<div class="form-group">
+						<label>Senha</label>
+						<input type="password" class="form-control" placeholder="********" name="password" required>
+					</div>
+					<button type="submit" class="btn btn-black">Acesse</button>
+					<a class="btn btn-black" href="{{ route('register.page') }}">Cadastre-se</a>
+				</form>
+			</div>
 		</div>
-	</div>
-
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-	 integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-	 integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-	 integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-
-</html>
+	</section>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,53 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+@extends('template.default', ['navbar' => false])
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="/css/app.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@section('title', 'Cadastro')
 
-	<title>Login</title>
-</head>
-
-<body>
-	<div class="sidenav">
-		<div class="login-main-text">
-			<h2>Faça seu login</h2>
-			<p>Ainda não faz parte do nosso time! Registre-se agora!</p>
+@section('content')
+	<section>
+		<div class="sidenav">
+			<div class="login-main-text">
+				<h2>Faça seu login</h2>
+				<p>Ainda não faz parte do nosso time! Registre-se agora!</p>
+			</div>
+			<img class="img" src="./assets/logo.png" alt="profile Pic" height="200" width="200">
 		</div>
-		<img class="img" src="./assets/logo.png" alt="profile Pic" height="200" width="200">
-	</div>
-	<div class="main">
-		<div class="col-md-6 col-sm-12">
-			<div class="register-form">
-				<form>
-					<div class="form-group">
-						<label>Nome Completo</label>
-						<input type="text" class="form-control" placeholder="Nome Completo">
-					</div>
-					<div class="form-group">
-						<label>Email</label>
-						<input type="email" class="form-control" placeholder="E-mail">
-					</div>
-					<div class="form-group">
-						<label>Telefone</label>
-						<input type="tel" class="form-control" placeholder="Telefone" size="13" maxlength="13">
-					</div>
-					<div class="form-group">
-						<label>CPF</label>
-						<input type="text" class="form-control" placeholder="CPF" size="11" maxlength="11">
-					</div>
-					<div class="form-group">
-						<label>Data de Nascimento</label>
-						<input type="date" class="form-control" placeholder="Data de Nascimento">
-					</div>
-					<div class="form-group">
-						<label>Endereço</label>
-						<input type="text" class="form-control" placeholder="Endereço">
-					</div>
-					<div class="form-group">
-						<label>CEP</label>
-						<input class="form-control" type="" size="8" maxlength="8">
-					</div>
-					<button type="submit" class="btn btn-black">Cadastrar</button>
-					<button type="submit" class="btn btn-balck">Cancelar</button>
-				</form>
+		<div class="main">
+			<div class="col-md-6 col-sm-12">
+				<div class="register-form">
+					<form>
+						<div class="form-group">
+							<label>Nome Completo</label>
+							<input type="text" class="form-control" placeholder="Nome Completo">
+						</div>
+						<div class="form-group">
+							<label>Email</label>
+							<input type="email" class="form-control" placeholder="E-mail">
+						</div>
+						<div class="form-group">
+							<label>Telefone</label>
+							<input type="tel" class="form-control" placeholder="Telefone" size="13" maxlength="13">
+						</div>
+						<div class="form-group">
+							<label>CPF</label>
+							<input type="text" class="form-control" placeholder="CPF" size="11" maxlength="11">
+						</div>
+						<div class="form-group">
+							<label>Data de Nascimento</label>
+							<input type="date" class="form-control" placeholder="Data de Nascimento">
+						</div>
+						<div class="form-group">
+							<label>Endereço</label>
+							<input type="text" class="form-control" placeholder="Endereço">
+						</div>
+						<div class="form-group">
+							<label>CEP</label>
+							<input class="form-control" type="" size="8" maxlength="8">
+						</div>
+						<button type="submit" class="btn btn-black">Cadastrar</button>
+						<button type="submit" class="btn btn-balck">Cancelar</button>
+					</form>
+				</div>
 			</div>
 		</div>
-	</div>
-
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-</body>
-
-</html>
+	</section>
+@endsection
