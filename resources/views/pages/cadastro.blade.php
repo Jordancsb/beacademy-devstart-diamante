dcfe52550ccaa6a932e23c1a50da3d555eea005d
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-07-06 08:08:11 -0300

    Adicionado name aos inputs e substituido o tipo dos valores para number's

--- resources/views/pages/cadastro.blade.php
+++ resources/views/pages/cadastro.blade.php
@@ -10,31 +10,31 @@
             @csrf
             <div class="form-group">
                <label for="name">Nome Completo</label>
-               <input type="text" class="form-control" id="name" required>
+               <input type="text" class="form-control" name="name" id="name" required>
             </div>
             <div class="form-group">
                <label for="description">Descrição</label>
-               <input type="text" class="form-control" id="description"  required>
+               <input type="text" class="form-control"  name="description" id="description"  required>
             </div>
             <div class="form-group">
                <label for="image_url">URL Imagem</label>
-               <input type="text" id="image_url" class="form-control"  required>
+               <input type="text"  name="image_url" id="image_url" class="form-control"  required>
             </div>
             <div class="form-group">
                <label for="size">Tamanho</label>
-               <input type="text" id="size" class="form-control" required>
+               <input type="text" name="size" id="size" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="quantity">Quantidade</label>
-                <input type="text" id="quantity" class="form-control" required>
+                <input type="text"   name="quantity" id="quantity" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="sale_price">Preço de Venda</label>
-                <input type="text" id="sale_price" class="form-control" required>
+                <input type="number"  name="sale_price" id="sale_price" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="cost_price">Preço de Custo</label>
-                <input type="text" id="cost_price" class="form-control" required>
+                <input type="number" name="cost_price" id="cost_price" class="form-control" required>
             </div>
             <button type="submit" class="btn btn-black">Cadastrar</button>
             <button type="submit" class="btn btn-black">Cancelar</button>
