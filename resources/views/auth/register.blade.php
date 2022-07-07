d11fe650feeb2f3ff502d2045bae7e5f8c59e019
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-07 10:59:53 -0300

    fix: modificações no form de cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,46 +3,64 @@
 @section('title', 'Cadastro')
 
 @section('content')
-	<section>
-		@include('partial.sidenav')
+    <section>
+        @include('partial.sidenav')
 
-		<div class="main">
-			<div class="col-md-6 col-sm-12">
-				<div class="register-form">
-					<form>
-						<div class="form-group">
-							<label>Nome Completo</label>
-							<input type="text" class="form-control" placeholder="Nome Completo">
-						</div>
-						<div class="form-group">
-							<label>Email</label>
-							<input type="email" class="form-control" placeholder="E-mail">
-						</div>
-						<div class="form-group">
-							<label>Telefone</label>
-							<input type="tel" class="form-control" placeholder="Telefone" size="13" maxlength="13">
-						</div>
-						<div class="form-group">
-							<label>CPF</label>
-							<input type="text" class="form-control" placeholder="CPF" size="11" maxlength="11">
-						</div>
-						<div class="form-group">
-							<label>Data de Nascimento</label>
-							<input type="date" class="form-control" placeholder="Data de Nascimento">
-						</div>
-						<div class="form-group">
-							<label>Endereço</label>
-							<input type="text" class="form-control" placeholder="Endereço">
-						</div>
-						<div class="form-group">
-							<label>CEP</label>
-							<input class="form-control" type="" size="8" maxlength="8">
-						</div>
-						<button type="submit" class="btn btn-black">Cadastrar</button>
-						<button type="submit" class="btn btn-balck">Cancelar</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	</section>
+        <div class="main">
+            <div class="col-md-6 col-sm-12">
+                <div class="register-form">
+                    <form method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="first-name">Nome</label>
+                            <input required type="text" class="form-control" placeholder="Nome" id="first-name"
+                                   name="first_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="last-name">Sobrenome</label>
+                            <input required type="text" class="form-control" placeholder="Sobrenome" id="last-name"
+                                   name="last_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input required type="email" class="form-control" placeholder="email@email.com" id="email"
+                                   name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Telefone</label>
+                            <input required type="text" class="form-control" placeholder="(00) 0 0000-0000"
+                                   pattern="[0-9]"
+                                   size="13"
+                                   maxlength="13" id="phone" name="phone">
+                        </div>
+                        <div class="form-group">
+                            <label for="cpf">CPF</label>
+                            <input required type="text" class="form-control" placeholder="000.000.000-00"
+                                   pattern="[0-9]"
+                                   size="11" maxlength="11" id="cpf" name="cpf">
+                        </div>
+                        <div class="form-group">
+                            <label for="birth-date">Data de Nascimento</label>
+                            <input required type="date" class="form-control" placeholder="00/00/0000" id="birth-date"
+                                   name="birth_date">
+                        </div>
+                        <div class="form-group">
+                            <label for="cep">CEP</label>
+                            <input required type="text" class="form-control" placeholder="00000-000" pattern="[0-9]"
+                                   size="8"
+                                   maxlength="8" id="cep"
+                                   name="cep">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Endereço</label>
+                            <input required type="text" class="form-control" placeholder="Endereço" id="address"
+                                   name="address">
+                        </div>
+                        <button type="submit" class="btn btn-black">Cadastrar</button>
+                        <a class="btn btn-black" href="{{ route('login.page') }}">Voltar</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
