36176504bf40eca9269d2bd98aa5fbc939f5bbc6
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-07 17:13:03 -0300

    `fix:` duas tabelas para usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,33 +4,43 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->id();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+			$table->string('first_name');
+			$table->string('last_name');
+
+			$table->string('phone');
+
+			$table->string('birth_date')->nullable();
+			$table->text('address')->nullable();
+			$table->timestamp('email_verified_at')->nullable();
+
+			$table->string('cpf')->unique();
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->boolean('admin')->default(false);
+
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
 };
