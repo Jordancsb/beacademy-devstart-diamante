c2763722ba2e08bf66dbd4db0b9d667556d3d6ac
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-07 20:57:50 -0300

    `feat:` método postNewUser funcional

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,33 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginFormRequest;
 use App\Models\User;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
-    function login()
-    {
-        return view('auth.login');
-    }
+	function getLoginPage()
+	{
+		return view('auth.login');
+	}
 
-    function register()
-    {
-        return view('auth.register');
-    }
+	function getRegisterPage()
+	{
+		return view('auth.register');
+	}
 
-    function checkout()
-    {
-        return view('auth.checkout');
-    }
+	function checkout()
+	{
+		return view('auth.checkout');
+	}
 
-    public function postLoginAuth(UserLoginFormRequest $req)
-    {
-        $queryParams = $req->only('email', 'password');
+	public function postLoginAuth(UserLoginFormRequest $req)
+	{
+		$queryParams = $req->only('email', 'password');
 
-        $user = User::where('email', $queryParams['email'])->get()[0] ?? null;
+		$user = User::where('email', $queryParams['email'])->get()[0] ?? null;
 
-        if ($user && password_verify($queryParams['password'], $user->password))
-            dd('authenticated');
+		if ($user && password_verify($queryParams['password'], $user->password))
+			dd('authenticated');
 
-        dd('not authenticated');
-    }
+		dd('not authenticated');
+	}
+
+	public function postNewUser(Request $req)
+	{
+		$data = $req->all();
+
+		User::create($data);
+	}
 }
