3c7bd7e4a2c8153ed62aabe5ec7bcb480d0828e8
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-19 16:20:15 -0300

    feat: github link

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="#">Diamante</a>
+		<a class="navbar-brand" href="https://github.com/Jordancsb/beacademy-devstart-diamante" target="_blank">Diamante</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
 			aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
