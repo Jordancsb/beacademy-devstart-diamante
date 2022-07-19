6cdca703623d631acfbc34b783f569b335565a6e
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-19 10:01:07 -0300

    feat: nova navbar

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,21 +1,40 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="#">Logo</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<a class="navbar-brand" href="#">Navbar</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+			aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link " href="/login">Login</a>
+					<a class="nav-link active" aria-current="page" href="{{ route('product.store') }}">Loja</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="/produto/cadastro">Cadastro de Produtos</a>
+					<a class="nav-link" href="#">Carrinho</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link " href="/cart">Pedidos</a>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+						data-bs-toggle="dropdown" aria-expanded="false">
+						Perfil
+					</a>
+					<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<li><a class="dropdown-item" href="#">Configurações</a></li>
+						<li><a class="dropdown-item" href="{{ route('logout') }}">Sair</a></li>
+					</ul>
 				</li>
+				@if (Auth::user()->admin)
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+							data-bs-toggle="dropdown" aria-expanded="false">
+							Gerenciar
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<li><a class="dropdown-item" href="#">Cadastrar Produto</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+						</ul>
+					</li>
+				@endif
 			</ul>
 		</div>
 	</div>
