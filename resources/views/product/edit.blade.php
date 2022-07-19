1856d4d81a0770402e09dff8be54af2ca24a16d7
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-19 16:05:38 -0300

    fix: layout

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -6,33 +6,47 @@
 		<form action="{{ route('products.update', $product->id) }}" method="post">
 			@method('PUT')
 			@csrf
-			<div class="mb-3">
-				<label for="name" class="form-label">Nome</label>
-				<input type="text" class="form-control" id="name" name="name" value="{{ $product->name }}">
+			<div class="row mb-2">
+				<div class="form-group col-sm-6">
+					<label for="name">Nome</label>
+					<input type="text" class="form-control" name="name" id="name" value="{{ $product->name }}" required>
+				</div>
+
+				<div class="form-group col-sm-6">
+					<label for="image_url">URL Imagem</label>
+					<input type="text" name="image_url" id="image_url" class="form-control" value="{{ $product->image_url }}"
+						required>
+				</div>
 			</div>
-			<div class="mb-3">
-				<label for="description" class="form-label">Descrição</label>
-				<textarea type="text" class="form-control" id="description" name="description">{{ $product->description }}</textarea>
+
+			<div class="form-group mb-2">
+				<label for="description">Descrição</label>
+				<textarea type="text" class="form-control" name="description" id="description" required>{{ $product->description }}</textarea>
 			</div>
-			<div class="mb-3">
-				<label for="image_url" class="form-label">Imagem</label>
-				<input type="text" class="form-control" id="image_url" name="image_url" value="{{ $product->image_url }}">
-			</div>
-			<div class="mb-3">
-				<label for="size" class="form-label">Tamanho</label>
-				<input type="number" class="form-control" id="size" name="size" value="{{ $product->size }}">
-			</div>
-			<div class="mb-3">
-				<label for="quantity" class="form-label">Quantidade</label>
-				<input type="number" class="form-control" id="quantity" name="quantity" value="{{ $product->quantity }}">
-			</div>
-			<div class="mb-3">
-				<label for="cost_price" class="form-label">Preço de Custo</label>
-				<input type="number" class="form-control" id="cost_price" name="cost_price" value="{{ $product->cost_price }}">
-			</div>
-			<div class="mb-3">
-				<label for="sale_price" class="form-label">Preço de Venda</label>
-				<input type="number" class="form-control" id="sale_price" name="sale_price" value="{{ $product->sale_price }}">
+
+			<div class="row mb-3">
+				<div class="form-group col-sm-3">
+					<label for="size">Tamanho</label>
+					<input type="number" name="size" id="size" class="form-control" value="{{ $product->size }}" required>
+				</div>
+
+				<div class="form-group col-sm-3">
+					<label for="quantity">Quantidade</label>
+					<input type="number" name="quantity" id="quantity" class="form-control" value="{{ $product->quantity }}"
+						required>
+				</div>
+
+				<div class="form-group col-sm-3">
+					<label for="sale_price">Preço de Venda</label>
+					<input type="number" name="sale_price" id="sale_price" class="form-control" value="{{ $product->sale_price }}"
+						required step="0.01">
+				</div>
+
+				<div class="form-group col-sm-3">
+					<label for="cost_price">Preço de Custo</label>
+					<input type="number" name="cost_price" id="cost_price" class="form-control" value="{{ $product->cost_price }}"
+						required step="0.01">
+				</div>
 			</div>
 			<a href="{{ url()->previous() }}" class="btn btn-outline-dark">Voltar</a>
 			<button type="submit" class="btn btn-dark">Atualizar</button>
