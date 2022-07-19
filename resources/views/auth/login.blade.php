0523cfb816320e113dcd900b62b3c2969ef7b506
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-19 10:21:12 -0300

    feat: novos forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,28 +3,27 @@
 @section('title', 'Login')
 
 @section('content')
-    <section>
-        @include('partial.sidenav')
+	<section>
+		@include('partial.sidenav')
 
-        <div class="main">
-            <div class="col-md-6 col-sm-12">
-                <form class="login-form" action="{{ route('auth.login') }}" method="POST">
-                    @include('partial.error')
-                    @csrf
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="text" class="form-control" placeholder="Email@email.com" id="email" name="email"
-                               required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Senha</label>
-                        <input type="password" class="form-control" placeholder="********" id="password" name="password"
-                               required>
-                    </div>
-                    <button type="submit" class="btn btn-black">Acesse</button>
-                    <a class="btn btn-black" href="{{ route('register.page') }}">Cadastre-se</a>
-                </form>
-            </div>
-        </div>
-    </section>
+		<div class="main">
+			<div class="col-md-6 col-sm-12">
+				<form class="login-form" action="{{ route('auth.login') }}" method="POST">
+					@include('partial.error')
+					@csrf
+					<div class="mb-3">
+						<label for="email" class="form-label">Email</label>
+						<input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+						<div id="emailHelp" class="form-text">Ainda não possui cadastro? Clique abaixo e inscreva-se!</div>
+					</div>
+					<div class="mb-3">
+						<label for="password" class="form-label">Senha</label>
+						<input type="password" class="form-control" id="password" name="password">
+					</div>
+					<button type="submit" class="btn btn-black">Entrar</button>
+					<a class="btn btn-outline-black" href="{{ route('register.page') }}">Cadastre-se</a>
+				</form>
+			</div>
+		</div>
+	</section>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,50 +7,61 @@
 		@include('partial.sidenav')
 
 		<div class="main">
-			<div class="col-md-6 col-sm-12">
+			<div class="col-md-8 col-sm-12">
 				<form method="POST" class="register-form" action="{{ route('users.create') }}">
+					@include('partial.error')
 					@csrf
-					<div class="form-group">
-						<label for="first-name">Nome</label>
-						<input required type="text" class="form-control" placeholder="Nome" id="first-name" name="first_name">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text">Nome Completo</span>
+						<input required type="text" class="form-control" placeholder="Nome" name="first_name">
+						<input required type="text" class="form-control" placeholder="Sobrenome" name="last_name">
 					</div>
-					<div class="form-group">
-						<label for="last-name">Sobrenome</label>
-						<input required type="text" class="form-control" placeholder="Sobrenome" id="last-name" name="last_name">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="email">Email</span>
+						<input aria-describedby="email" required type="email" class="form-control" placeholder="seu@email.com"
+							id="email" name="email">
 					</div>
-					<div class="form-group">
-						<label for="email">Email</label>
-						<input required type="email" class="form-control" placeholder="email@email.com" id="email" name="email">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="password">Senha</span>
+						<input aria-describedby="password" required type="password" class="form-control" placeholder="********"
+							id="password" name="password">
 					</div>
-					<div class="form-group">
-						<label for="password">Senha</label>
-						<input required type="password" class="form-control" placeholder="********" id="password" name="password">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="phone">Telefone</span>
+						<input aria-describedby="phone" required type="text" class="form-control" placeholder="(00) 0 0000-0000"
+							pattern="[0-9]+" size="13" maxlength="13" id="phone" name="phone">
 					</div>
-					<div class="form-group">
-						<label for="phone">Telefone</label>
-						<input required type="text" class="form-control" placeholder="(00) 0 0000-0000" pattern="[0-9]+" size="13"
-							maxlength="13" id="phone" name="phone">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="cpf">CPF</span>
+						<input aria-describedby="cpf" required type="text" class="form-control" placeholder="000.000.000-00"
+							pattern="[0-9]+" size="11" maxlength="11" id="cpf" name="cpf">
 					</div>
-					<div class="form-group">
-						<label for="cpf">CPF</label>
-						<input required type="text" class="form-control" placeholder="000.000.000-00" pattern="[0-9]+" size="11"
-							maxlength="11" id="cpf" name="cpf">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="birth-date">Data de Nascimento</span>
+						<input aria-describedby="birth-date" required type="date" class="form-control" placeholder="00/00/0000"
+							id="birth-date" name="birth_date">
 					</div>
-					<div class="form-group">
-						<label for="birth-date">Data de Nascimento</label>
-						<input required type="date" class="form-control" placeholder="00/00/0000" id="birth-date" name="birth_date">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="cep">CEP</span>
+						<input aria-describedby="cep" required type="text" class="form-control" placeholder="00000-000" pattern="[0-9]+"
+							size="8" maxlength="8" id="cep" name="cep">
 					</div>
-					<div class="form-group">
-						<label for="cep">CEP</label>
-						<input required type="text" class="form-control" placeholder="00000-000" pattern="[0-9]+" size="8"
-							maxlength="8" id="cep" name="cep">
+
+					<div class="input-group flex-nowrap mb-2">
+						<span class="input-group-text" id="address">Endereço</span>
+						<input aria-describedby="address" required type="text" class="form-control" placeholder="Endereço"
+							id="address" name="address">
 					</div>
-					<div class="form-group">
-						<label for="address">Endereço</label>
-						<input required type="text" class="form-control" placeholder="Endereço" id="address" name="address">
-					</div>
+
+					<a class="btn btn-outline-black" href="{{ route('login') }}">Voltar</a>
 					<button type="submit" class="btn btn-black">Cadastrar</button>
-					<a class="btn btn-black" href="{{ route('login') }}">Voltar</a>
 				</form>
 			</div>
 		</div>
