5a585d6306681fa8cbc9b110793a2987ad3918b0
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-19 14:04:38 -0300

    feat: trativa de usúario não logado

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -7,34 +7,43 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
 			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="{{ route('product.store') }}">Loja</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Carrinho</a>
-				</li>
-				@if (Auth::user()->admin)
+				@if (Auth::check())
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="{{ route('product.store') }}">Loja</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Carrinho</a>
+					</li>
+					@if (Auth::user()->admin)
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+								data-bs-toggle="dropdown" aria-expanded="false">
+								Gerenciar
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<li><a class="dropdown-item" href="{{ route('product.new') }}">Cadastrar Produto</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+							</ul>
+						</li>
+					@endif
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
 							data-bs-toggle="dropdown" aria-expanded="false">
-							Gerenciar
+							Perfil
 						</a>
 						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-							<li><a class="dropdown-item" href="{{ route('product.new') }}">Cadastrar Produto</a></li>
-							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li><a class="dropdown-item" href="#">Configurações</a></li>
+							<li><a class="dropdown-item" href="{{ route('logout') }}">Sair</a></li>
 						</ul>
 					</li>
+				@else
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="{{ route('login') }}">Entrar</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('register.page') }}">Cadastrar</a>
+					</li>
 				@endif
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-						data-bs-toggle="dropdown" aria-expanded="false">
-						Perfil
-					</a>
-					<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<li><a class="dropdown-item" href="#">Configurações</a></li>
-						<li><a class="dropdown-item" href="{{ route('logout') }}">Sair</a></li>
-					</ul>
-				</li>
 			</ul>
 		</div>
 	</div>
--- resources/views/product/store.blade.php
+++ resources/views/product/store.blade.php
@@ -3,57 +3,56 @@
 @section('title', 'Loja')
 
 @section('content')
-    <section class="container justify-content-center mt-5">
-        <div class=" mt-2 mb-5">
-            <form action="{{ route('product.store') }}" method="GET">
-                <div class="input-group">
-                    <input type="search" class="form-control rounded" name="search" />
-                    <button type="submit" class="btn btn-outline-dark">Pesquisar</button>
-                </div>
-            </form>
-        </div>
-        <div class="row gy-3">
-            @foreach ($products as $product)
-            <div class="col-md-6 mb-3">
-                <div class="card mb-3 h-100" style="max-width: 540px;">
-                    <div class="row ">
-                        <div class="col-md-4">
-                            <img src="{{ $product->image_url }} " class="card-img-top" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title"> {{ $product->name }} </h5>
-                                <p class="card-text overflow-hidden">
-                                    {{ $product->description }} </p>
-                                <div class="card-text">
-                                    <div class="row">
-                                        <div class="col">
-                                            <b> <strong>Tamanho: </strong> <span
-                                                    class="badge rounded-pill bg-info text-dark"> {{ $product->size }}
-                                                </span>
-                                            </b>
-                                        </div>
-                                        <div class="col">
-                                            <b> <strong style="color: red"> R$
-                                                </strong> <span style="color: green"> {{ $product->cost_price }}</span>
-                                            </b>
-                                        </div>
-                                    </div>
-                                </div>
-                                <form action="{{ route('product.cart', $product->id) }}" method="post" class="mt-3 mb-0">
-                                    <button type="submit" class="btn btn-outline-dark " style="width: 100%;"> Carrinho
-                                    </button>
-                                </form>
+	<section class="container justify-content-center mt-5">
+		<div class=" mt-2 mb-5">
+			<form action="{{ route('product.store') }}" method="GET">
+				<div class="input-group">
+					<input type="search" class="form-control rounded" name="search" />
+					<button type="submit" class="btn btn-outline-dark">Pesquisar</button>
+				</div>
+			</form>
+		</div>
+		<div class="row gy-3">
+			@foreach ($products as $product)
+				<div class="col-md-6 mb-3">
+					<div class="card mb-3 h-100" style="max-width: 540px;">
+						<div class="row ">
+							<div class="col-md-4">
+								<img src="{{ $product->image_url }} " class="card-img-top" alt="...">
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title"> {{ $product->name }} </h5>
+									<p class="card-text overflow-hidden">
+										{{ $product->description }} </p>
+									<div class="card-text">
+										<div class="row">
+											<div class="col">
+												<b> <strong>Tamanho: </strong> <span class="badge rounded-pill bg-info text-dark"> {{ $product->size }}
+													</span>
+												</b>
+											</div>
+											<div class="col">
+												<b> <strong style="color: red"> R$
+													</strong> <span style="color: green"> {{ $product->cost_price }}</span>
+												</b>
+											</div>
+										</div>
+									</div>
+									<form action="{{ route('product.cart', $product->id) }}" method="post" class="mt-3 mb-0">
+										<button type="submit" class="btn btn-outline-dark " style="width: 100%;"> Carrinho
+										</button>
+									</form>
 
-                            </div>
-                        </div>
-                    </div>
-                </div>
+								</div>
+							</div>
+						</div>
+					</div>
 
-            </div>
-            @endforeach
-        </div>
+				</div>
+			@endforeach
+		</div>
 
 
-    </section>
+	</section>
 @endsection
