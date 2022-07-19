009714bad43706cedf8ea43269a16985787978ea
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-19 10:59:19 -0300

    feat: ajustes na view product.create

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -3,40 +3,43 @@
 @section('title', 'Cadastro de Produtos')
 
 @section('content')
-<section class="container d-flex mt-4 ">
-		
-		<form action="{{ route('products.create') }}" class="form-cadastro-produto" method="POST">
+	<section class="container mt-4">
+		<h1>Novo Produto</h1>
+
+		<form action="{{ route('products.create') }}" method="POST" class="col col-sm-10">
 			@csrf
-			<div class="form-group">
-				<label for="name">Nome Completo</label>
+			<div class="form-group mb-2">
+				<label for="name">Nome</label>
 				<input type="text" class="form-control" name="name" id="name" required>
 			</div>
-			<div class="form-group">
+			<div class="form-group mb-2">
 				<label for="description">Descrição</label>
 				<input type="text" class="form-control" name="description" id="description" required>
 			</div>
-			<div class="form-group">
+			<div class="form-group mb-2">
 				<label for="image_url">URL Imagem</label>
 				<input type="text" name="image_url" id="image_url" class="form-control" required>
 			</div>
-			<div class="form-group">
+			<div class="form-group mb-2">
 				<label for="size">Tamanho</label>
 				<input type="number" name="size" id="size" class="form-control" required>
 			</div>
-			<div class="form-group">
+			<div class="form-group mb-2">
 				<label for="quantity">Quantidade</label>
 				<input type="number" name="quantity" id="quantity" class="form-control" required>
 			</div>
-			<div class="form-group">
-				<label for="sale_price">Preço de Venda</label>
-				<input type="number" name="sale_price" id="sale_price" class="form-control" required step="0.01">
+			<div class="row mb-2">
+				<div class="form-group col-sm-6">
+					<label for="sale_price">Preço de Venda</label>
+					<input type="number" name="sale_price" id="sale_price" class="form-control" required step="0.01">
+				</div>
+				<div class="form-group col-sm-6">
+					<label for="cost_price">Preço de Custo</label>
+					<input type="number" name="cost_price" id="cost_price" class="form-control" required step="0.01">
+				</div>
 			</div>
-			<div class="form-group">
-				<label for="cost_price">Preço de Custo</label>
-				<input type="number" name="cost_price" id="cost_price" class="form-control" required step="0.01">
-			</div>
-			<button type="submit" class="btn btn-black">Cadastrar</button>
-			<button type="submit" class="btn btn-black">Cancelar</button>
+			<button type="submit" class="btn btn-outline-dark">Cancelar</button>
+			<button type="submit" class="btn btn-dark">Cadastrar</button>
 		</form>
 	</section>
 @endsection
