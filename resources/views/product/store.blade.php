9273ad1c69dd168f3bb440956370ea4e4e63dd3a
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-21 09:44:33 -0300

    style: atualizar layout da loja

--- resources/views/product/store.blade.php
+++ resources/views/product/store.blade.php
@@ -4,51 +4,48 @@
 
 @section('content')
 	<section class="container justify-content-center mt-5">
-		<div class=" mt-2 mb-5">
-			<form action="{{ route('product.store') }}" method="GET">
-				<div class="input-group">
-					<input type="search" class="form-control rounded" name="search" />
-					<button type="submit" class="btn btn-outline-dark">Pesquisar</button>
-				</div>
-			</form>
-		</div>
-		<div class="row gy-3">
+		<form action="{{ route('product.store') }}" method="GET">
+			@csrf
+			<div class="input-group mb-3">
+				<input type="text" class="form-control" placeholder="Pesquisar..." aria-label="Pesquisar..."
+					aria-describedby="searchAddon" name="search">
+				<button class="btn btn-outline-dark" type="submit" id="searchAddon">Pesquisar</button>
+			</div>
+		</form>
+
+
+		<div class="row row-cols-1 row-cols-md-3 g-4">
 			@foreach ($products as $product)
-				<div class="col-md-6 mb-3">
-					<div class="card mb-3 h-100" style="max-width: 540px;">
+				<div class="col-md-6">
+					<div class="card mb-3">
 						<div class="row ">
 							<div class="col-md-4">
 								<img src="{{ $product->image_url }} " class="card-img-top" alt="...">
 							</div>
 							<div class="col-md-8">
 								<div class="card-body">
-									<h5 class="card-title"> {{ $product->name }} </h5>
-									<p class="card-text overflow-hidden">
-										{{ $product->description }} </p>
-									<div class="card-text">
-										<div class="row">
-											<div class="col">
-												<b> <strong>Tamanho: </strong> <span class="badge rounded-pill bg-info text-dark"> {{ $product->size }}
-													</span>
-												</b>
-											</div>
-											<div class="col">
-												<b> <strong style="color: red"> R$
-													</strong> <span style="color: green"> {{ $product->cost_price }}</span>
-												</b>
-											</div>
+									<h5 class="card-title">{{ $product->name }}</h5>
+									<p class="card-text">{{ $product->description }}</p>
+
+									<div class="row">
+										<div class="col">
+											<p class="card-text"><strong>Tamanho: </strong>
+												<span class="badge rounded-pill bg-secondary">{{ $product->size }}</span>
+											</p>
+										</div>
+										<div class="col">
+											<p class="card-text"><strong>R$</strong> {{ $product->cost_price }}</p>
 										</div>
 									</div>
-									<form action="{{ route('product.cart', $product->id) }}" method="post" class="mt-3 mb-0">
-										<button type="submit" class="btn btn-outline-dark " style="width: 100%;"> Carrinho
-										</button>
-									</form>
 
+									<form action="{{ route('product.cart', $product->id) }}" method="post" class="mt-3 mb-0">
+										@csrf
+										<button type="submit" class="btn btn-outline-dark">Carrinho</button>
+									</form>
 								</div>
 							</div>
 						</div>
 					</div>
-
 				</div>
 			@endforeach
 		</div>
