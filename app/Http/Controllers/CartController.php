30ff872f1c976156a82520be4fa43903b19bdf52
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-28 14:39:34 -0300

    refactor: remoção de use CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Product;
 
 class CartController extends Controller
 {
