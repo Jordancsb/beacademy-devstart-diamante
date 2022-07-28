38bfbb84c680c34f135d548657203ef1dac99abf
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-28 12:42:48 -0300

    fix: use duplicado em web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\CartController;
 
