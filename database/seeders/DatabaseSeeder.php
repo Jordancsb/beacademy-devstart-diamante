25ff00af45a57af51a7908c712aab2cc46b47558
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-08-03 10:02:06 -0300

    refactor: DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,9 +22,6 @@ class DatabaseSeeder extends Seeder
 		\App\Models\Order::factory(5)->create();
 
 		\App\Models\User::factory()->create([
-			'first_name' => 'Admin',
-			'last_name' => 'Admin',
-			'phone' => '(00) 0 0000-0000',
 			'email' => 'admin@email.com',
 			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 			'admin' => true
