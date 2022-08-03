7dc2a0c278e27357a348c0c3a59005ac82407ecc
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-08-03 18:37:03 -0300

    feat: cron job template

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            echo('[LOG] Email Cron-Job.');
+        })->everyMinute();
     }
 
     /**
@@ -25,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
