b74874afc5c6f4edffaa67433ed88aa487a494fd
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-08-09 16:20:33 -0300

    tests: AddressControllerTest.php

--- tests/Feature/app/Http/Controllers/AddressControllerTest.php
+++ tests/Feature/app/Http/Controllers/AddressControllerTest.php
@@ -12,6 +12,43 @@ class AddressControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function testNotAuthenticatedUserCanNotUnlinkAnAddress()
+    {
+        // PREPARE
+        $user = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $user->id]);
+
+        // ACT
+        $response = $this->delete(route('addresses.delete', $address->id));
+
+        // ASSERT
+        $this->assertDatabaseCount(Address::class , 1);
+        $this->assertDatabaseHas(Address::class , [
+            'id' => $address->id,
+            'user_id' => $user->id
+        ]);
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testUserCanNotUnlinkAnotherUserAddress()
+    {
+        // PREPARE
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $otherUser->id]);
+
+        // ACT
+        $this->actingAs($user);
+        $this->delete(route('addresses.delete', $address->id));
+
+        // ASSERT
+        $this->assertDatabaseCount(Address::class , 1);
+        $this->assertDatabaseHas(Address::class , [
+            'id' => $address->id,
+            'user_id' => $address->user_id
+        ]);
+    }
+
     public function testUserCanUnlinkAnAddressToHisAccount()
     {
         // PREPARE
@@ -32,6 +69,21 @@ class AddressControllerTest extends TestCase
         ]);
     }
 
+    public function testNotAuthenticatedUserCanNotCreateANewAddress()
+    {
+        // PREPARE
+        User::factory()->create();
+        $payload = Address::factory()->definition();
+        $payload['postcode'] = '00000000';
+
+        // ACT
+        $this->post(route('addresses.create'), $payload);
+
+        // ASSERT
+        $this->assertDatabaseCount(Address::class , 0);
+        $this->assertDatabaseMissing(Address::class , $payload);
+    }
+
     public function testUserCanCreateANewAddress()
     {
         // PREPARE
@@ -48,11 +100,47 @@ class AddressControllerTest extends TestCase
         $this->assertDatabaseHas(Address::class , $payload);
     }
 
+    public function testNotAuthenticatedUserCanNotEditAddress()
+    {
+        // PREPARE
+        $user = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $user->id]);
+        $payload = Address::factory()->definition();
+        $payload['postcode'] = '00000000';
+
+        // ACT
+        $this->put(route('addresses.update', $address->id), $payload);
+
+        // ASSERT
+        $this->assertDatabaseCount(Address::class , 1);
+        $this->assertDatabaseMissing(Address::class , $payload);
+        $this->assertDatabaseHas(Address::class , $address->toArray());
+    }
+
+    public function testUserCanNotEditAnotherUserAddress()
+    {
+        // PREPARE
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $otherUser->id]);
+        $payload = Address::factory()->definition();
+        $payload['postcode'] = '00000000';
+
+        // ACT
+        $this->actingAs($user);
+        $this->put(route('addresses.update', $address->id), $payload);
+
+        // ASSERT
+        $this->assertDatabaseCount(Address::class , 1);
+        $this->assertDatabaseMissing(Address::class , $payload);
+        $this->assertDatabaseHas(Address::class , $address->toArray());
+    }
+
     public function testUserCanEditAddress()
     {
         // PREPARE
         $user = User::factory()->create();
-        $address = Address::factory()->create();
+        $address = Address::factory()->create(['user_id' => $user->id]);
         $payload = Address::factory()->definition();
         $payload['postcode'] = '00000000';
 
